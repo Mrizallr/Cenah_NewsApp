125ff383285eddc1547bab80ec97f1c29a083eec
Mrizallr <2206082@itg.ac.id>
2025-05-30 21:42:01 +0700

    feat:membuat file dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cenah New App',
+      title: 'Cenah News App',
       theme: ThemeData(
         // This is the theme of your application.
         //

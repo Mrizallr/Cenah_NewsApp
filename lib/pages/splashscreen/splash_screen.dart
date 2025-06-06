9d21125ff3488660c582e2fb351ceac0277ae2c7
Mrizallr <2206082@itg.ac.id>
2025-06-06 20:21:04 +0700

    featch: slicing splashscreen

--- lib/pages/splashscreen/splash_screen.dart
+++ lib/pages/splashscreen/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cenah_news/pages/onboarding/onboarding_screen.dart';
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,15 +12,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Navigasi ke halaman intro setelah 3 detik
-    Future.delayed(const Duration(seconds: 3), () {
-      // PERBAIKAN: Tambahkan pemeriksaan 'mounted'
-      if (!mounted) return; // Jika widget sudah tidak ada, jangan lakukan navigasi
-
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => const IntroductionScreen(),
-        ),
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const onboardingScreen()),
       );
     });
   }
@@ -28,22 +23,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Sesuaikan warna latar belakang jika perlu
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Logo Cenah News
-            Image.asset(
-              'assets/images/logo.png', // Pastikan nama file cocok dengan yang di pubspec.yaml
-              width: 150, // Sesuaikan ukuran logo
-              height: 150, // Sesuaikan ukuran logo
-            ),
-            const SizedBox(height: 30), // Spasi antara logo dan indikator
-            // Indikator loading
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // Sesuaikan warna indikator
-            ),
+            Image.asset('assets/images/logo.png', width: 150),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(color: Colors.indigo),
           ],
         ),
       ),

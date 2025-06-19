0fed05e499b499cac852dc2f563447e79baaa0a7
Mrizallr <2206082@itg.ac.id>
2025-06-19 22:02:49 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,65 @@
-import 'package:cenah_news/pages/splashscreen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:cenah_news/src/configs/app_routes.dart';
+import 'package:cenah_news/src/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<AuthProvider>(context, listen: false).initAuth();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      color: Colors.white,
-      title: 'Cenah News',
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF007BFF), // warna biru dari tulisan NEWS
-          brightness: Brightness.light,
-        ),
-      ),
+    return Consumer<AuthProvider>(
+      builder: (context, authProvider, child) {
+        return ScreenUtilInit(
+          designSize: const Size(360, 960),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (_, child) {
+            return MaterialApp(
+              title: 'Kelompok 1',
+              debugShowCheckedModeBanner: false,
+              initialRoute: AppRoutes.splash,
+              onGenerateRoute: AppRoutes.generateRoute,
+              theme: ThemeData(
+                primaryColor: const Color(0xFF2C3E50),
+                colorScheme: ColorScheme.fromSwatch().copyWith(
+                  secondary: const Color(0xFF3498DB),
+                ),
+                fontFamily: 'Poppins',
+              ),
+              supportedLocales: [
+                Locale('en'), // English
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }

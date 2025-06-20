e577e1647908493f31f408b61aa921ac19afbcf5
Mrizallr <2206082@itg.ac.id>
2025-06-20 23:20:55 +0700

    update: app routes

--- lib/src/configs/app_routes.dart
+++ lib/src/configs/app_routes.dart
@@ -1,3 +1,6 @@
+import 'package:cenah_news/src/pages/create_article/create_article_screen.dart';
+import 'package:cenah_news/src/pages/edit_article/edit_article_screen.dart';
+import 'package:cenah_news/src/pages/my_articles/my_articles.dart';
 import 'package:flutter/material.dart';
 import 'package:cenah_news/src/pages/home/home_screen.dart';
 import 'package:cenah_news/src/pages/auth/login_screen.dart';
@@ -27,6 +30,15 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case home:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case myArticles:
+        return MaterialPageRoute(builder: (_) => const MyArticlesScreen());
+      case createArticle:
+        return MaterialPageRoute(builder: (_) => const CreateArticleScreen());
+      case editArticle:
+        final articleId = settings.arguments as String;
+        return MaterialPageRoute(
+          builder: (_) => EditArticleScreen(articleId: articleId),
+        );
       default:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
     }
